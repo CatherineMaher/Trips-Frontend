9d33ec19cb951383c56eb0a27e594e889c2f9cdf
aliaa2001 <aliaaehab50@gmail.com>
2022-11-09 18:18:09 +0200

    trip & station CRUD operation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminLoginComponent } from './admin-login/admin-login.component';
 import { AdminSignupComponent } from './admin-signup/admin-signup.component';
+import { TripComponent } from './trip/trip.component';
+import { StationComponent } from './station/station.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     AdminLoginComponent,
-    AdminSignupComponent
+    AdminSignupComponent,
+    TripComponent,
+    StationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot([
+      {path:"admin-login", component:AdminLoginComponent},
+      {path:"admin-signup", component:AdminSignupComponent},
+      {path:"trip", component:TripComponent},
+      {path:'station',component: StationComponent},
+      {path:' ', redirectTo:'admin-signup', pathMatch:'full'},
+      {path: '**' , redirectTo:'admin-signup', pathMatch:'full'}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
